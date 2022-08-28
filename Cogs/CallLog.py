414127b7266f1049b2c5c63caacb7985ea46b405
Secon0101 <duwnsgur11012@gmail.com>
2022-08-28 18:36:29 +0900

    임베드 함수로 분리

--- Cogs/CallLog.py
+++ Cogs/CallLog.py
@@ -51,13 +51,9 @@ class CallLog(commands.Cog):
             self.update_call_log(member.id, ActionType.LEAVE, before.channel)
     
     
-    @commands.slash_command(name="통계", description="모든 유저의 최근 통화방 접속 기록을 조회합니다.")
-    async def slash_view_stats(self, ctx: discord.ApplicationContext,
-        time_count: discord.Option(int, "최근 n시간의 기록 조회 (수가 클 경우 임베드가 잘릴 수 있음)", min_value=1, max_value=24, default=12)
-    ):
-        log(f"{CallLog.__name__} - {ctx.author.name}({ctx.author.id})(이)가 /{ctx.command.name} 사용")
-        embed = discord.Embed(description="*임베드 생성 중...*", color=0x78b159)
-        respond = await ctx.respond(embed=embed)
+    async def make_timeline_embed(self, time_count: int) -> discord.Embed:
+        """ 통화방 접속 기록 임베드를 생성한다. """
+        embed = discord.Embed(title=f"최근 {time_count}시간의 통화방 접속 기록", color=0x78b159)
         call_log = self.get_call_log()
         
         INTERVAL = 60 * 60  # 한 시간 간격
@@ -68,6 +64,7 @@ class CallLog(commands.Cog):
         
         def add_state(user_id: str, action: ActionType):
             """ 이번 시간의 통화 상태를 기록하고 t에 INTERVAL을 더한다. """
+            nonlocal t
             match action:
                 case ActionType.JOIN:
                     timeline[user_id] += '🟩'
@@ -79,6 +76,7 @@ class CallLog(commands.Cog):
                     timeline[user_id] += '⬛'
                 case ActionType.UNKNOWN:
                     timeline[user_id] += '▪️'
+            t += INTERVAL
         
         ## 모든 유저의 시간대별 상태 기록
         for user_id in call_log.keys():
@@ -93,14 +91,12 @@ class CallLog(commands.Cog):
                     if t + INTERVAL <= int(action_time):
                         while t < int(action_time) - int(action_time) % INTERVAL:
                             add_state(user_id, last_state[user_id])
-                            t += INTERVAL
                     
                     ## 이벤트가 일어난 전후에는 반드시 JOIN이 존재한다 (예외: 정시에 퇴장)
                     if actions[action_time]["action"] == ActionType.LEAVE and int(action_time) % INTERVAL == 0:
                         add_state(user_id, ActionType.LEAVE)
                     else:
                         add_state(user_id, ActionType.JOIN)
-                    t += INTERVAL
                 
                 ## 이전 상태 저장
                 last_state[user_id] = ActionType(actions[action_time]["action"])
@@ -108,51 +104,6 @@ class CallLog(commands.Cog):
             ## 마지막 액션 이후 시간은 이전 상태 유지
             while t <= current:
                 add_state(user_id, last_state[user_id])
-                t += INTERVAL
-        
-        """ # // (아래는 이전 코드)
-        for t in range(current - TIME_COUNT * INTERVAL, current, INTERVAL):
-            for id in call_log.keys():  # 유저마다
-                
-                ## 시간 구간 내에 있는 모든 액션 가져오기
-                actions_in_range: list[dict] = []
-                for i in range(len(call_log[id])):
-                    if t <= call_log[id][i]["time"]:
-                        if t + INTERVAL <= call_log[id][i]["time"]:
-                            break
-                        actions_in_range.append(call_log[id][i])
-                    else:
-                        last_state[id] = ActionType(call_log[id][i]["action"])
-                
-                ## 대푯값 산출하기
-                if len(actions_in_range) > 0:
-                    time_by_action = { ActionType.JOIN: 0, ActionType.LEAVE: 0 }
-                    for i in reversed(range(-1, len(actions_in_range))):  # 다음 액션 시각 - 현재 액션 시각 = 현재 액션 지속 시간
-                        next_time = actions_in_range[i+1]["time"] if i+1 < len(actions_in_range) else t + INTERVAL
-                        current_time = actions_in_range[i]["time"] if i >= 0 else t
-                        action = actions_in_range[i]["action"] if i >= 0 else 1 - actions_in_range[i+1]["action"]
-                        time_by_action[ActionType(action)] += next_time - current_time
-                        
-                    state = max(time_by_action, key=time_by_action.get)
-                    last_state[id] = ActionType(actions_in_range[-1]["action"])
-                else:
-                    state = last_state[id]
-                
-                ## 액션을 타임라인에 저장
-                match state:
-                    case ActionType.JOIN:
-                        timeline[id] += '🟩'
-                    case ActionType.OTHER_SERVER:
-                        timeline[id] += '🟧'
-                    case ActionType.LEAVE:
-                        timeline[id] += '⬛'
-                    case ActionType.UNKNOWN:
-                        timeline[id] += '▪️'
-        """
-        
-        ## 출력
-        embed.title = f"최근 {time_count}시간의 통화방 접속 기록"
-        embed.description = None
         
         user_ids = [id for id in call_log.keys() if joined[id]]  # 접속했던 유저만 표시
         users = []
@@ -171,6 +122,18 @@ class CallLog(commands.Cog):
         
         embed.set_footer(text="🟩 통화 중  ⬛ 나감  ▪️ 알 수 없음", icon_url=self.bot.user.display_avatar.url, )
         embed.timestamp = datetime.now(timezone('Asia/Seoul'))
+        
+        return embed
+    
+    
+    @commands.slash_command(name="통계", description="모든 유저의 최근 통화방 접속 기록을 조회합니다.")
+    async def slash_view_stats(self, ctx: discord.ApplicationContext,
+        time_count: discord.Option(int, "최근 n시간의 기록 조회 (수가 클 경우 임베드가 잘릴 수 있음)", min_value=1, max_value=24, default=12)
+    ):
+        log(f"{CallLog.__name__} - {ctx.author.name}({ctx.author.id})(이)가 /{ctx.command.name} 사용")
+        embed = discord.Embed(description="*임베드 생성 중...*", color=0x78b159)
+        respond = await ctx.respond(embed=embed)
+        embed = await self.make_timeline_embed(time_count)
         await respond.edit_original_message(embed=embed)
 
 
