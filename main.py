d421f3c530e5c2e6daffabd6a5baee626cebe9a1
세컨 <duwnsgur11012@gmail.com>
2023-03-26 00:29:36 +0900

    서버에서 파일을 못 찾는 문제 수정

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import discord
-from utility import get_cogs, log
-from dotenv import load_dotenv
-from os import getenv
-from sys import argv
+import sys
+import os
 import firebase_admin as firebase
+from dotenv import load_dotenv
+from utility import get_cogs, log
 
 
 log("로딩...", newline=True)
 
-dev = len(argv) >= 2 and argv[1] == "dev"
+dev = len(sys.argv) >= 2 and sys.argv[1] == "dev"
 bot = discord.Bot(owner_ids=[540481950763319317, 718285849888030720],
     debug_guilds=[1086542872607723550] if dev else None, intents=discord.Intents.all())
 
 # 데이터베이스 로드
+os.chdir(sys.path[0])
 cred = firebase.credentials.Certificate('firebase-admin.json')
 firebase.initialize_app(cred, { "databaseURL": "https://nosookbot-default-rtdb.firebaseio.com" })
 
@@ -21,7 +22,7 @@ bot.load_extensions(*get_cogs())
 
 # 토큰 로드
 load_dotenv()
-token = getenv('TOKEN_ALPHA' if dev else 'TOKEN')
+token = os.getenv('TOKEN_ALPHA' if dev else 'TOKEN')
 
 log("로그인 중...")
 bot.run(token)
