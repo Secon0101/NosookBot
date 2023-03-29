5e262d8370c6b7ceafc74a0cc9e5d2fbdd28313f
세컨 <duwnsgur11012@gmail.com>
2023-03-29 15:05:24 +0900

    로그 추가

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from utility import log_print, get_cogs
 
 
-log_print("Loading...", newline=True)
+log_print("Preparing Bot...", newline=True)
 
 # 테스트 모드이면 True (> python main.py dev)
 dev = len(argv) >= 2 and argv[1] == "dev"
@@ -19,14 +19,18 @@ bot = discord.Bot(owner_ids=[540481950763319317, 718285849888030720],
 load_dotenv()
 
 # 파이어베이스
+log_print("Loading Firebase...")
 fb_admin_file = "firebase-admin.json"
 if not exists(fb_admin_file):
+    log_print("firebase-admin.json not found, creating...")
     with open(fb_admin_file, 'w') as f:
         f.write(b64decode(getenv("FIREBASE_ADMIN_BASE64")).decode("utf-8"))
+    log_print("Created firebase-admin.json")
 
 cred = firebase.credentials.Certificate(fb_admin_file)
 options = { "databaseURL": getenv("DATABASE_URL") }
 firebase.initialize_app(cred, options)
+log_print("Firebase loaded")
 
 # 커맨드 불러오기
 bot.load_extensions(*get_cogs())
